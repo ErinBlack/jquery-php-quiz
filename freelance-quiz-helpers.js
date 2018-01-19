773b2ad6d6fffdecf0afbb53dab7ec453417a317
Erin Black <erin.black@fieldnation.com>
2018-01-19 09:28:42 -0600

    adding first roiund of files to github repo

---
+++ freelance-quiz-helpers.js
@@ -0,0 +1,117 @@
+// All Freelance Quiz Questions
+const quizQuestions = [
+    {
+        question: 1,
+        answers: {
+            a: 'Networking is not my strong suit. Reaching out to secure projects is challenging for me.',
+            b: 'I’m good at networking. I enjoy establishing and maintaining professional relationships.',
+        }
+    },
+    {
+        question: 2,
+        answers: {
+            a: 'Working with multiple clients does not faze me.',
+            b: 'I prefer to work with one client at a time.',
+        }
+    },
+    {
+        question: 3,
+        answers: {
+            a: 'I prefer working structured, routine days 40-hours per week.',
+            b: 'I prefer a work schedule that varies from day-to-day.',
+        }
+    },
+    {
+        question: 4,
+        answers: {
+            a: 'I like working out of the same office every day.',
+            b: 'I don’t mind working from different locations, be it my home office or even on-the-go.',
+        }
+    },
+    {
+        question: 5,
+        answers: {
+            a: 'I am good at following written instructions and communicating with clients virtually.',
+            b: 'I prefer to communicate with clients face-to-face, and have a hard time following written instructions.',
+        }
+    },
+    {
+        question: 6,
+        answers: {
+            a: 'I am okay with rejection- it’s a part of doing business.',
+            b: 'I do not handle rejection well.',
+        }
+    },
+    {
+        question: 7,
+        answers: {
+            a: 'Receiving a regular paycheck for the same amount each pay period is important to me.',
+            b: 'I am okay with receiving irregular paychecks that are based on the amount of work I do.',
+        }
+    },
+    {
+        question: 8,
+        answers: {
+            a: 'I need a boss or supervisor to help motivate me.',
+            b: 'I would describe myself as self-motivated.',
+        }
+    },
+    {
+        question: 9,
+        answers: {
+            a: 'The flexibility to create my own schedule is important to me.',
+            b: 'I don’t mind working 40 hours a week with a predetermined amount of vacation and sick time.',
+        }
+    },
+    {
+        question: 10,
+        answers: {
+            a: 'I don’t mind completing any task assigned to me.',
+            b: 'I like having the autonomy to choose which projects I take on.',
+        }
+    },
+];
+
+// All Freelance Quiz Answers
+const quizAnswerKey = [
+    {
+        name: '1',
+        ccorrectAnswer: 'b'
+    },
+    {
+        name: '2',
+        correctAnswer: 'a'
+    },
+    {
+        name: '3',
+        correctAnswer: 'b'
+    },
+    {
+        name: '4',
+        correctAnswer: 'b'
+    },
+    {
+        name: '5',
+        correctAnswer: 'a'
+    },
+    {
+        name: '6',
+        correctAnswer: 'a'
+    },
+    {
+        name: '7',
+        correctAnswer: 'b'
+    },
+    {
+        name: '8',
+        correctAnswer: 'b'
+    },
+    {
+        name: '9',
+        correctAnswer: 'a'
+    },
+    {
+        name: '10',
+        correctAnswer: 'b'
+    }
+]; // end quizAnswerKey
---
+++ quiz.js
@@ -0,0 +1,49 @@
+$(function() {
+    // buildQuiz();
+    quizBuilder.buildQuiz();
+
+    $( "#js-quiz__submit" ).click(function( event ) {
+        quizBuilder.serializeResults(event);
+    });
+});
+
+const answers = [];
+
+const quizBuilder = {
+    buildQuiz : function (){
+        const quizOutput = [];
+
+        quizQuestions.forEach(
+            (currentQuestion, questionNumber) => {
+                const answers = [];
+                const currentQuestionNum = currentQuestion.question;
+                // HTML to print question number
+                const questionHTML = `<h3>Question ${currentQuestionNum}</h3>`;
+                // Loop through all answer choices
+                for(var letter in currentQuestion.answers){
+                    const currentAnswer = currentQuestion.answers[letter];
+                    // HTML to print quiz answers
+                    var answersHtml = `<div class="c-quizAnswer"><input type="radio"
+                        name="${currentQuestionNum}" value="${letter}" id="${letter}">
+                        <label for="${letter}"> ${currentAnswer}</label></div>`;
+                    answers.push(answersHtml);
+                } // end currentQuestions forLoop
+                // pushing all code into the questionHTML array
+                const htmlJoin = questionHTML + answers.join('');
+                quizOutput.push(htmlJoin);
+            } //end quizQuestions forEach loop
+        ); // end quizQuestions forEach
+        const quizOutputJoin = quizOutput.join('');
+        $(quizOutputJoin).prependTo('.c-quiz__form');
+    },// end buildQuiz function
+    // Serializing quiz results
+    serializeResults : function(event){
+        event.preventDefault();
+        const quizResults = $("#js-quiz__form").serializeArray();
+        quizBuilder.parseResults(quizResults);
+    },
+    parseResults : function(quizResults){
+        console.log('in parseResults with', quizResults);
+        console.log('in parseResults with', quizAnswerKey);
+    }
+}; // end quizBuilder
